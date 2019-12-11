45c31a4ceb60f2f8d90b1a926c940ed33800e99b
Justyna Mazur-Kochmańska <justyna.p.mazur@gmail.com>
2019-12-11 22:33:15 +0100

    Add test scenario: login with valid credentials (with pytest-variables support)

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,12 +8,21 @@ class TestLogin():
 	@pytest.fixture
 	def driver(self, request):
 		driver_ = webdriver.Firefox()
-
+		driver_.maximize_window()
+		
 		def quit():
 			driver_.quit()
 
 		request.addfinalizer(quit)
 		return driver_
 
-	def test_invalid_password(self, driver):
+	def test_login_with_valid_credentials(self, driver, variables):
 		driver.get("https://github.com/login")
+		driver.find_element(By.ID, "login_field").send_keys(variables['valid_username'])
+		driver.find_element(By.ID, "password").send_keys(variables['valid_password'])
+		driver.find_element(By.NAME, "commit").click()
+
+		driver.find_element(By.CSS_SELECTOR, ".dashboard-sidebar").is_displayed()
+		driver.find_element(By.XPATH, "/html/body/div[1]/header/div[7]/details/summary").click()
+		msg_text = driver.find_element(By.TAG_NAME, "strong").text		
+		assert msg_text == variables['valid_username']
