415fac66135e0303077f10dfd70e181a7c9bc561
Justyna Mazur-Kochmańska <justyna.p.mazur@gmail.com>
2019-12-11 23:31:22 +0100

    Add test scenario: login with invalid password

--- tests/test_login.py
+++ tests/test_login.py
@@ -26,3 +26,12 @@ class TestLogin():
 		driver.find_element(By.XPATH, "/html/body/div[1]/header/div[7]/details/summary").click()
 		msg_text = driver.find_element(By.TAG_NAME, "strong").text		
 		assert msg_text == variables['valid_username']
+
+	def test_login_with_invalid_password(self, driver, variables):
+		driver.get("https://github.com/login")
+		driver.find_element(By.ID, "login_field").send_keys(variables['valid_username'])
+		driver.find_element(By.ID, "password").send_keys(variables['invalid_password'])
+		driver.find_element(By.NAME, "commit").click()
+
+		msg_text = driver.find_element(By.CSS_SELECTOR, ".flash-error").text
+		assert msg_text == "Incorrect username or password."
